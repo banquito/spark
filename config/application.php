e961fc77e3540a56984ce161cb1d0e2786a216d3
Clodo <claudiobidau@gmail.com>
2015-07-24 11:58:51 -0300

    update wordpress version

--- config/application.php
+++ config/application.php
@@ -1,19 +1,24 @@
 <?php
 $root_dir = dirname(__DIR__);
 $webroot_dir = $root_dir . '/web';
+
 /**
  * Use Dotenv to set required environment variables and load .env file in root
  */
+$dotenv = new Dotenv\Dotenv($root_dir);
 if (file_exists($root_dir . '/.env')) {
-  Dotenv::load($root_dir);
+  $dotenv->load();
+  $dotenv->required(['DB_NAME', 'DB_USER', 'DB_PASSWORD', 'WP_HOME', 'WP_SITEURL']);
 }
-Dotenv::required(['DB_NAME', 'DB_USER', 'DB_PASSWORD', 'WP_HOME', 'WP_SITEURL']);
+
 /**
  * Set up our global environment constant and load its config first
  * Default: development
  */
 define('WP_ENV', getenv('WP_ENV') ?: 'development');
+
 $env_config = __DIR__ . '/environments/' . WP_ENV . '.php';
+
 if (file_exists($env_config)) {
   require_once $env_config;
 }
